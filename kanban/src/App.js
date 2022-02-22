77b9a06ba9958a45cf000ebc53a62a35362d077f
Victorcsilva <95497623+Victorcsilva@users.noreply.github.com>
2022-02-22 01:01:04 -0300

    Atualização de estlização de estilização de textos

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -1,20 +1,54 @@
 import './App.css';
-import Card from './components/Card/Card';
+import Card from './components/Card/Card'
 
 function App() {
   return (
     
-    <div className="App">
-      <header className="App-header">
+ <div className="container">
       <p class="kanban_text">Kanban</p>
-      <div className="card_todo">
-        <Card/>
-        <Card/>
-        </div>
-      
-     </header>
-      
-    </div>
+    <div className="tasks">
+      <div className="Card_ToDo">
+           <Card>Christmas Banners
+             <div>
+           <span className="label">Label</span>
+           </div>
+            </Card>
+            <Card>Redo Portfolio
+            <span className="label">Label</span>
+            </Card>
+      </div>
+      <div className="Card_inProgress">
+        <Card>Coffee Break
+        <span className="label_orange">Always</span>
+        </Card>
+        <Card>Updating Portfolio 
+        <span className="label_orange">Webflow</span>
+        </Card>  
+      </div>
+      <div className="Card_Review">
+        <Card>Release to Figma Community
+        <span className="label_purple">Release</span>
+        </Card>
+        <Card>User Feedback
+        <span className="label_purple">Feedback</span>
+          </Card>  
+        <Card>Background images from humaaans.com
+        <span className="label_purple">Sourcing</span>
+        </Card>  
+      </div>
+      <div className="Card_Done">
+        <Card>Style Guide
+        <span className="label_green">UI</span>
+        </Card>
+        <Card>Component Library
+        <span className="label_green">UI</span>
+        </Card>  
+        <Card>Sticker Components
+        <span className="label_green">UI</span>
+          </Card>  
+      </div>  
+  </div>    
+ </div>
     
   );
 }
--- kanban/src/components/Card/Card.js
+++ kanban/src/components/Card/Card.js
@@ -1,15 +1,17 @@
 import './Card.css'
 import avatarM from '../../components/icon/man.png'
 const Card = (props) => { 
+  const { children,color,status } = props
  return (
      <div className="card_label">
-     <p className="text1_style">Christmas Banners</p>
+     <p className="text1_style">{children}</p>
      <div className="label_div">
      <img src={avatarM} className="avatar_male" alt="avatar"></img>
-     <span className="label">Label</span>
+     <span className={`label${color}`}>{status}</span>
      </div>
     </div>
   )
 }
+
 
 export default Card
