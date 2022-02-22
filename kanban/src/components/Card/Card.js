ff26711f8e9f7c8963a22781c3f8550e6c294e09
Victorcsilva <95497623+Victorcsilva@users.noreply.github.com>
2022-02-22 01:15:05 -0300

    FINALIZAÇÃO/ALELUIA

--- kanban/src/components/Card/Card.js
+++ kanban/src/components/Card/Card.js
@@ -1,17 +1,12 @@
 import './Card.css'
-import avatarM from '../../components/icon/man.png'
 const Card = (props) => { 
   const { children,color,status } = props
  return (
      <div className="card_label">
      <p className="text1_style">{children}</p>
-     <div className="label_div">
-     <img src={avatarM} className="avatar_male" alt="avatar"></img>
      <span className={`label${color}`}>{status}</span>
-     </div>
     </div>
   )
 }
-
 
 export default Card
--- kanban/src/components/Label_card/Label_card.js
+++
@@ -1,9 +0,0 @@
-import './Label_card.css'
-
-
-const Label_card = (children) => {
-  return (
-    <span className="card_label"></span>
-  )
-}
-export default Label_card
