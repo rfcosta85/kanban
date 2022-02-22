3ef7216ac1e1891f5f2e9e6acda0f2025a770480
Victorcsilva <95497623+Victorcsilva@users.noreply.github.com>
2022-02-21 22:55:13 -0300

    Atualização de estlização de card principal1

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -1,22 +1,15 @@
-import back from './Home.png';
 import './App.css';
 import Card from './components/Card/Card';
-import avatar from './components/icon/female.png'
-import avatar2 from './components/icon/man.png'
-import Label from './components/Label_card/Label_card'
-
 
 function App() {
   return (
     
     <div className="App">
       <header className="App-header">
-      <p class="kanban_text">Kanban </p>
-      <div className="Card_div">
-        <Card>
-        <p className="text1_style">Christmas Banners</p>
-         <img src={avatar} className="avatar_male" alt="avatar" />
-        </Card>
+      <p class="kanban_text">Kanban</p>
+      <div className="card_todo">
+        <Card/>
+        <Card/>
         </div>
       
      </header>
--- kanban/src/components/Card/Card.js
+++ kanban/src/components/Card/Card.js
@@ -1,10 +1,13 @@
 import './Card.css'
-const Card = (props) => {
-  const { children = 'Label'} = props
- 
-  return (
-     <div className="card_label">{children}
-     <span className="label"></span>
+import avatarM from '../../components/icon/man.png'
+const Card = (props) => { 
+ return (
+     <div className="card_label">
+     <p className="text1_style">Christmas Banners</p>
+     <div className="label_div">
+     <img src={avatarM} className="avatar_male" alt="avatar"></img>
+     <span className="label">Label</span>
+     </div>
     </div>
   )
 }
